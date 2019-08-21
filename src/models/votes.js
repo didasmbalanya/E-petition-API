7fb12d9171eb5f431282aeee14da61ada6ce7083
Mucyo Fred <mucyomiller@gmail.com>
2019-08-21 21:47:16 +0200

    chore(DB,API): adds models & finish project structures - added models and their relationships - added others required directories - added projects routes files - added generic Validation class - moved migrations & seeders dir into database dir [finishes #168039867]

--- src/models/votes.js
+++ src/models/votes.js
@@ -1,13 +1,13 @@
 export default (sequelize, DataTypes) => {
   const votes = sequelize.define('votes', {
-    id: DataTypes.INTEGER,
     petition_id: DataTypes.INTEGER,
     user_id: DataTypes.INTEGER,
     vote: DataTypes.BOOLEAN,
   }, {});
   votes.associate = (models) => {
     votes.belongsToMany(models.users, {
-      foreignKey: 'user_id',
+      // foreignKey: 'user_id',
+      through: 'UserVote',
     });
     votes.belongsTo(models.petitions, {
       foreignKey: 'petition_id',
