aee6629d5a5a41dc7a157c02243b9eeb5c1d8460
Didas Mbalanya <didas.mbalanya@Didass-MacBook-Pro.local>
2019-08-22 19:37:56 +0200

    set up auth.js

--- src/middlewares/user/auth.js
+++ src/middlewares/user/auth.js
@@ -1,15 +1,21 @@
+/* eslint-disable import/no-unresolved */
 import jwt from 'jsonwebtoken';
+import userService from '../services/UserServices';
 
 // eslint-disable-next-line consistent-return
-export default (req, res, next) => {
-  const token = req.header('x-auth-token');
+export const auth = async (req, res, next) => {
+  const token = req.header('Authorization').replace('Bearer ', '');
   if (!token) return res.json({ status: 401, message: 'Access denied, No token provided' });
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
-    req.user = decoded;
+    const decoded = jwt.verify(token, process.env.secret);
+    const verifiedUser = await userService.findUserByEmail(decoded.email);
+    if (!verifiedUser) throw Error();
+    req.user = verifiedUser;
     next();
-  } catch (ex) {
-    res.json({ status: 400, message: 'invalid token' });
+  } catch (error) {
+    res.status(401).send({ status: 401, message: 'invalid token' });
   }
 };
+
+export default auth;
