79ce762520129cad3450239aa5ecfcac89ba120f
Didas Mbalanya <didas.mbalanya@Didass-MacBook-Pro.local>
2019-08-22 18:21:17 +0200

    add sign up route,controller,service and utils

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
+import userController from '../controllers/userController';
 
 const router = new Router();
 
+router.post('/signUp', userController.signUp);
 
 export default router;
