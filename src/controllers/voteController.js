c4c9e8e03f7c308e77f88ba24b3f9e0eb18c85c3
RUGUMBIRA Jordy Bastien <jordybastien@JORDY-BASTIENs-MacBook-Pro.local>
2019-08-23 09:03:10 +0200

    feature(API): User can vote for or against a petition [finishes #167980341]

--- src/controllers/voteController.js
+++ src/controllers/voteController.js
@@ -1,33 +1,40 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable indent */
+import dotenv from 'dotenv';
 import voteService from '../services/VoteService';
 import PetitionService from '../services/PetitionServices';
 import Util from '../utils/Utils';
+import db from '../models';
 
 const util = new Util();
 
 class voteController {
   // eslint-disable-next-line consistent-return
   static async addVote(req, res) {
-        if (!req.body.user_id || !req.body.vote || !req.params) {
+        if (!req.body.vote || !req.params) {
           util.setError(400, 'Please provide complete details');
           return util.send(res);
         }
+        const petition = await db.petitions.findOne({ where: { id: Number(req.params.id) } });
+        if (!petition) {
+            util.setError(400, 'Petition does not exist');
+            return util.send(res);
+            }
         // eslint-disable-next-line camelcase
-        const { vote, user_id } = req.body;
+        const { vote } = req.body;
         // userId get from token
         const newId = req.params;
         // eslint-disable-next-line camelcase
         const petition_id = newId.id;
         const newVote = {
             petition_id,
-            user_id,
+            user_id: req.user.dataValues.id,
             vote,
         };
         const alteredVote = req.body;
         try {
             // search if user already voted on certain petition
-            const theVote = await voteService.findVote(user_id, petition_id);
+            const theVote = await voteService.findVote(req.user.dataValues.id, petition_id);
             if (theVote) {
                 if (theVote.dataValues.vote === true) {
                     if (req.body.vote === 'true') {
@@ -37,7 +44,7 @@ class voteController {
                     if (req.body.vote === 'false') {
                         const updatePetition = await PetitionService.downVote(petition_id);
                         // eslint-disable-next-line max-len
-                        const updateVote = await voteService.updateVote(user_id, petition_id, alteredVote);
+                        const updateVote = await voteService.updateVote(req.user.dataValues.id, petition_id, alteredVote);
                         util.setSuccess(200, 'Vote updated!', updateVote);
                         return util.send(res);
                     }
@@ -46,7 +53,7 @@ class voteController {
                     if (req.body.vote === 'true') {
                         const updatePetition = await PetitionService.upVote(petition_id);
                         // eslint-disable-next-line max-len
-                        const updateVote = await voteService.updateVote(user_id, petition_id, alteredVote);
+                        const updateVote = await voteService.updateVote(req.user.dataValues.id, petition_id, alteredVote);
                         util.setSuccess(200, 'Vote updated!', updateVote);
                         return util.send(res);
                     } if (req.body.vote === 'false') {
--- src/middlewares/user/auth.js
+++ src/middlewares/user/auth.js
@@ -1,11 +1,13 @@
 /* eslint-disable import/no-unresolved */
 import jwt from 'jsonwebtoken';
-import userService from '../services/UserServices';
+import userService from '../../services/userServices';
 
 // eslint-disable-next-line consistent-return
 export const auth = async (req, res, next) => {
+  const header = req.header('Authorization');
+  if (!header) return res.json({ status: 401, message: 'Access denied, No token provided' });
+
   const token = req.header('Authorization').replace('Bearer ', '');
-  if (!token) return res.json({ status: 401, message: 'Access denied, No token provided' });
 
   try {
     const decoded = jwt.verify(token, process.env.secret);
