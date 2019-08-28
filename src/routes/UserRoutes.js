11a08816517586bc61a1abe63beefd6ca18a2c73
Mucyo Fred <mucyomiller@gmail.com>
2019-08-28 20:09:46 +0200

    feature(API): user can sign in with facebook [finishes #168168874]

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-named-as-default */
 import { Router } from 'express';
+import passport from 'passport';
 import UserController from '../controllers/userController';
 import Validation from '../validation/validation';
 import wrongMethod from '../middlewares/router/wrongMethod';
@@ -8,10 +9,14 @@ import auth from '../middlewares/user/auth';
 const router = new Router();
 const { userValidator, signInValidator } = Validation;
 
-const { signIn, signUp, changeAdmin } = UserController;
+const {
+  signIn, fbSignIn, signUp, changeAdmin,
+} = UserController;
 
 router.route('/auth/signup').post(userValidator, signUp).all(wrongMethod);
 router.route('/auth/signin').post(signInValidator, signIn).all(wrongMethod);
 router.route('/admin/:id').patch(auth, changeAdmin).all(wrongMethod);
+router.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email'] }));
+router.get('/auth/fb/callback', passport.authenticate('facebook', { failureRedirect: '/api/v1/auth/facebook' }), fbSignIn);
 
 export default router;
